add5476957732f80e5ff3dfac40c3d8bf5a3bc82
EzeLRA <ezeleocraft@gmail.com>
2025-06-17 16:26:22 -0300

    first commit

---
+++ src/client/java/com/prueba/PruebaModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.prueba;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PruebaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
